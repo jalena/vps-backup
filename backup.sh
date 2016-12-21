8340a65fb9b4c212310da859af4416b289416064
jalena <jalena@bcsytv.com>
2016-12-21 23:16:57 +0800

    fix
    fix bug

--- backup.sh
+++ backup.sh
@@ -9,34 +9,16 @@
 [[ $EUID -ne 0 ]] && echo 'Error: This script must be run as root!' && exit 1
 
 # global variables
-<<<<<<< HEAD
 current_date=`date +%Y%m%d`
-=======
-variables(){
-	current_date=`date +%Y%m%d`
-}
->>>>>>> origin/master
 
 # Initialize the database of account information
 function initialization(){
 	if [[ ! -e '/root/.my.cnf' ]]; then
-<<<<<<< HEAD
 		echo -e "\e[34mPlease enter the MySQL user (Default : \e[33mroot):\e[0m"
 		read -p "Please enter：" MYSQL_USER
 		[[ -z "$MYSQL_USER" ]] && MYSQL_USER="root"
 		echo -e "\e[34mPlease enter the MySQL password:\e[0m"
 		read -p "Please enter：" MYSQL_PASS
-=======
-		echo "Please enter the MySQL user (Default : root):\n"
-		read -p MYSQL_USER
-		[[ -z "$MYSQL_USER" ]] && MYSQL_USER="root"
-		echo "Please enter the MySQL password:\n"
-		read -p MYSQL_PASS
-		echo -e "---------------------------"
-		echo -e "MySQL User = $MYSQL_USER"
-		echo -e "MySQL Pass = $MYSQL_PASS"
-		echo -e "---------------------------"
->>>>>>> origin/master
 cat > /root/.my.cnf<<EOF
 [client]
 user=$MYSQL_USER
@@ -46,7 +28,6 @@ password=$MYSQL_PASS
 user=$MYSQL_USER
 password=$MYSQL_PASS
 EOF
-<<<<<<< HEAD
 		echo -e "------------------------------------------------------"
 		echo -e "MySQL User = $MYSQL_USER"
 		echo -e "MySQL Pass = $MYSQL_PASS"
@@ -83,37 +64,6 @@ function initialization_check(){
 		source /root/.backup.option
 	elif [[ -s '/root/.my.cnf' ]]; then
 		source /root/.my.cnf
-=======
-	fi
-
-	if [[ ! -e '.backup.option' ]]; then
-		echo "Please enter backup path:\n"
-		read -p BACKUP
-			[[ -z $BACKUP ]] && BACKUP = "/data/backup"
-		echo "Please enter the web Path (Default : /data/wwwroot):\n"
-		read -p WEB_PATH
-			[[ -z $WEB_PATH ]] && WEB_PATH="/data/wwwroot"
-		echo "Please enter the nginx configuration path (Default : /usr/local/nginx/conf/vhost):\n"
-		read -p NGINX_PATH
-			[[ -z $NGINX_PATH ]] && NGINX_PATH="/usr/local/nginx/conf/vhost"
-		echo -e "---------------------------"
-		echo -e "Backup directory = $WEB_PATH"
-		echo -e "nginx directory = $NGINX_PATH"
-		echo -e "---------------------------"
-cat > /root/.backup.option<<EOF
-BACKUP_DIR=$BACKUP
-WEB_PATH=$WEB_PATH
-NGINX_PATH=$NGINX_PATH
-EOF
-	fi
-}
-
-initialization_check(){
-	if [[ -s '/root/.backup.option']]; then
-		source "/root/.backup.option"
-	elif [[ -s '/root/.my.cnf' ]]; then
-		source "/root/.my.cnf"
->>>>>>> origin/master
 	else
 		initialization
 	fi
@@ -124,23 +74,15 @@ initialization_check(){
 }
 
 # Backup all database tables
-<<<<<<< HEAD
 function backup_database(){
 	for db in $(mysql -B -N -e 'SHOW DATABASES' |sed -e '/_schema/d' -e '/mysql/d' -e '/sys/d')
 		do
 			mysqldump ${db} | gzip -9 > ${BACKUP_DIR}/${db}.sql.gz
-=======
-backup_database(){
-	for db in $(mysql -B -N -e 'SHOW DATABASES' |sed -e '/_schema/d' -e '/mysql/d' -e '/sys/d')
-		do
-			mysqldump ${db} | gzip -9 - > ${BACKUP_DIR}/${db}.sql.gz
->>>>>>> origin/master
 			echo -e "\t\e[1;32m--- Backup data table \e[1;31m${db} \e[1;32msuccess! ---\e[0m"
 	done
 
 	# Pack all database tables
-<<<<<<< HEAD
-	tar zcPf mysql_${current_date}.tar.gz ${BACKUP_DIR}/*.sql.gz --remove-files
+	tar zcPf ${BACKUP_DIR}/sql_${current_date}.tar.gz ${BACKUP_DIR}/*.sql.gz --remove-files
 }
 
 # Packing site data
@@ -148,108 +90,72 @@ function packing_data(){
 	for web in $(ls -1 ${WEB_PATH} |sed -e '/phpMy/d')
 	do
 		tar zcPf ${BACKUP_DIR}/${web}_${current_date}.tar.gz ${WEB_PATH}/${web}
-=======
-	tar zcf mysql_$current_date.tar.gz *.sql.gz --remove-files
-}
-
-# Packing site data
-packing_data(){
-	for web in $(ls -1 ${WEB_PATH} |sed -e '/phpMy/d')
-	do
-		tar zcPf ${BACKUP_DIR}/${web}_$current_date.tar.gz ${WEB_PATH}/${web}
->>>>>>> origin/master
 		echo -e "\t\e[1;32m--- package \e[1;31m${web} \e[1;32msuccess! ---\e[0m"
 	done
 }
 
 # package the nginx configuration file
-<<<<<<< HEAD
 function configuration(){
 	tar cPf ${BACKUP_DIR}/nginx_${current_date}.tar.gz $NGINX_PATH
 	echo -e "\t\e[1;32m--- package \e[1;31mnginx_${current_date}.tar.gz \e[1;32msuccess! ---\e[0m"
 	find / -name nginx.conf |grep -v root | xargs tar rPf ${BACKUP_DIR}/nginx_${current_date}.tar.gz
-=======
-configuration(){
-	tar zcPf ${BACKUP_DIR}/nginx_$current_date.tar.gz $NGINX_PATH
-	echo -e "\t\e[1;32m--- package \e[1;31mnginx_$current_date.tar.gz \e[1;32msuccess! ---\e[0m"
-	find / -name nginx.conf |grep -v root | xargs tar rPf ${BACKUP_DIR}/nginx_$current_date.tar.gz
->>>>>>> origin/master
 	echo -e "\t\e[1;32m--- Additional file successfully ---\e[0m"
 }
 
 # Upload data
-<<<<<<< HEAD
 function upload_file(){
-=======
-upload_file(){
-	variables
->>>>>>> origin/master
 	for file in $(ls -1 ${BACKUP_DIR})
 		do
 			#scp ${file} root@23.239.196.3:/root/backup/${file}
 			#sh /root/dropbox_uploader.sh upload ${file} backup/${file}
-<<<<<<< HEAD
 			qshell fput backup ${file} ${BACKUP_DIR}/${file} http://up.qiniug.com
-=======
-			./qshell fput backup ${file} ${BACKUP_DIR}/${file} http://up.qiniug.com
->>>>>>> origin/master
 	done
 }
 
 # Restore all data
-<<<<<<< HEAD
 function restore_all(){
-=======
-restore_all(){
->>>>>>> origin/master
-	initialization_check
-	cd ${BACKUP_DIR}
-	tar zxf mysql*.tar.gz
-	for db in $(find *.sql.gz | sed 's/.sql.gz//g')
+	level=`tar -ztPf sql*.tar.gz | grep -o '/' | wc -l`
+	num=level-1
+	tar zxf sql*.tar.gz --strip-components ${num}
+	for db in $(find ${BACKUP_DIR}/*.sql.gz | sed 's/.sql.gz//g')
 		do
-			mysqladmin create ${db}
-			gunzip -f < ${db}.sql.gz | mysql ${db}
+			# mysqladmin create ${db}
+			# gunzip -f < ${BACKUP_DIR}/${db}.sql.gz | mysql ${db}
 	done
 
-	for web in $(ls -1 *.tar.gz| grep -v mysql |grep -v nginx)
+	for web in $(ls -1 ${BACKUP_DIR}/*.tar.gz| grep -v mysql |grep -v nginx)
 		do
-			tar zxPf ${web}
+			tar zxPf ${BACKUP_DIR}/${web}
+
 	done
 
-	for nginx in $(ls -1 nginx*)
+	for nginx in $(ls -1 ${BACKUP_DIR}/nginx*)
 		do
-			tar zxPf ${nginx}
+			tar zxPf ${BACKUP_DIR}/${nginx}
 	done
 }
 
 # Initialization settings
-<<<<<<< HEAD
 function initial_setup(){
 	initialization
 }
 
 function backup_db(){
-=======
-initial_setup(){
-	initialization
-}
-
-backup_db(){
->>>>>>> origin/master
 	initialization_check
 	backup_database
 }
 
 # Full backup
-<<<<<<< HEAD
 function backup_all(){
-=======
-backup_all(){
->>>>>>> origin/master
 	initialization_check
 	backup_database
 	packing_data
 	configuration
+	upload_file
+}
+
+function upload(){
+	initialization_check
 	upload_file
 }
 
@@ -267,7 +173,7 @@ db)
 	backup_db
 	;;
 up)
-	upload_file
+	upload
 	;;
 Restore)
 	restore_all
